b2d21a74ff835b6143c5be89eaac84eae8baee40
BlazikenSGU <truongnhat.nguyen.37@gmail.com>
2024-07-11 23:30:53 +0700

    five

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,14 +2,13 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class Welcome extends CI_Controller 
+class IndexController extends CI_Controller 
 {
 	public function index()
 	{
 
 		$this->load->model('IndexModel');
-		$data['allproduct'] = $this->IndexModel->getallproduct();
-		var_dump($data);
+		
 
 		$this->load->view('layout');
 	}
